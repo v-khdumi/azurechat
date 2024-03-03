c622f04d834239de5e690f5edea5fe079c67b024
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2024-03-04 01:13:12 +0200

    Update login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { AI_NAME } from "@/features/theme/theme-config";
 import { signIn } from "next-auth/react";
 import { FC } from "react";
 import { Avatar, AvatarImage } from "../ui/avatar";
@@ -16,28 +15,28 @@ interface LoginProps {
   isDevMode: boolean;
 }
 
-export const LogIn: FC<LoginProps> = (props) => {
+export const LogIn: FC<LoginProps> = ({ isDevMode }) => {
   return (
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
         <CardTitle className="text-2xl flex gap-2">
           <Avatar className="h-8 w-8">
-            <AvatarImage src={"ai-icon.png"} />
+            <AvatarImage src={"ai-icon.png"} alt="AI Icon" />
           </Avatar>
-          <span className="text-primary">{AI_NAME}</span>
+          <span className="text-primary">Your App Name</span> {/* Utilizează AI_NAME dacă este definit */}
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Login with your Personal Account or Microsoft 365 account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        {props.isDevMode ? (
-          <Button onClick={() => signIn("localdev")}>
+        <Button onClick={() => signIn("azure-ad-b2c")}>Personal Account</Button>
+        <Button onClick={() => signIn("azure-ad")}>Microsoft 365</Button>
+        {isDevMode && (
+          <Button onClick={() => signIn("credentials")}>
             Basic Auth (DEV ONLY)
           </Button>
-        ) : null}
+        )}
       </CardContent>
     </Card>
   );
