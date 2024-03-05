462d7b8beec1ed674fc81dc5c0e818d21c44e695
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2024-03-05 22:58:57 +0200

    Update login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AI_NAME } from "@/features/theme/theme-config";
 import { signIn } from "next-auth/react";
 import { FC } from "react";
 import { Avatar, AvatarImage } from "../ui/avatar";
@@ -15,28 +16,27 @@ interface LoginProps {
   isDevMode: boolean;
 }
 
-export const LogIn: FC<LoginProps> = ({ isDevMode }) => {
+export const LogIn: FC<LoginProps> = (props) => {
   return (
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
         <CardTitle className="text-2xl flex gap-2">
           <Avatar className="h-8 w-8">
-            <AvatarImage src={"ai-icon.png"} alt="AI Icon" />
+            <AvatarImage src={"ai-icon.png"} />
           </Avatar>
-          <span className="text-primary">AnyAsk</span> {/* Utilizează AI_NAME dacă este definit */}
+          <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login with your Personal Account or Microsoft 365 account
+          Login in with your Personal or Microsoft 365 account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("azure-ad-b2c")}>Personal Account</Button>
-        <Button onClick={() => signIn("azure-ad")}>Microsoft 365</Button>
-        {isDevMode && (
-          <Button onClick={() => signIn("credentials")}>
+        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        {props.isDevMode ? (
+          <Button onClick={() => signIn("localdev")}>
             Basic Auth (DEV ONLY)
           </Button>
-        )}
+        ) : null}
       </CardContent>
     </Card>
   );
