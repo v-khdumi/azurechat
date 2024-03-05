0ff7715ca8c9f0427a0950501a9786a07e4db1a7
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2024-03-05 23:00:20 +0200

    Update helpers.ts

--- src/features/auth-page/helpers.ts
+++ src/features/auth-page/helpers.ts
@@ -1,34 +1,22 @@
 import { createHash } from "crypto";
-import { getServerSession } from "next-auth/next";
-import { options } from "./auth-api";
+import { getServerSession } from "next-auth";
 import { RedirectToPage } from "../common/navigation-helpers";
+import { options } from "./auth-api";
 
-
-// Definiția tipului pentru modelul de utilizator
-export type UserModel = {
-  name: string;
-  image: string;
-  email: string;
-  isAdmin: boolean;
-};
-
-// Obținerea sesiunii curente a utilizatorului și returnarea unui model simplificat de utilizator
 export const userSession = async (): Promise<UserModel | null> => {
-  // Asigură-te că pasăm contextul corect necesar pentru `getServerSession`
-  const session = await getServerSession({ req }, { res }, options);
+  const session = await getServerSession(options);
   if (session && session.user) {
     return {
       name: session.user.name!,
       image: session.user.image!,
       email: session.user.email!,
-      isAdmin: session.user.isAdmin ?? false, // Presupunem false dacă nu este setat
+      isAdmin: session.user.isAdmin!,
     };
   }
 
   return null;
 };
 
-// Obține utilizatorul curent sau aruncă o eroare dacă nu este găsit
 export const getCurrentUser = async (): Promise<UserModel> => {
   const user = await userSession();
   if (user) {
@@ -37,7 +25,6 @@ export const getCurrentUser = async (): Promise<UserModel> => {
   throw new Error("User not found");
 };
 
-// Generează un ID hashat pentru utilizator bazat pe email
 export const userHashedId = async (): Promise<string> => {
   const user = await userSession();
   if (user) {
@@ -47,17 +34,22 @@ export const userHashedId = async (): Promise<string> => {
   throw new Error("User not found");
 };
 
-// Funcție helper pentru a hash-ui o valoare folosind SHA-256
 export const hashValue = (value: string): string => {
   const hash = createHash("sha256");
   hash.update(value);
   return hash.digest("hex");
 };
 
-// Redirecționează utilizatorul autentificat către o pagină specifică
-export const redirectIfAuthenticated = async (req, res) => {
-  const user = await userSession(req, res);
+export const redirectIfAuthenticated = async () => {
+  const user = await userSession();
   if (user) {
-    RedirectToPage("chat", res);
+    RedirectToPage("chat");
   }
+};
+
+export type UserModel = {
+  name: string;
+  image: string;
+  email: string;
+  isAdmin: boolean;
 };
